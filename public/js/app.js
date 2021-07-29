4107c865b4ba22a74cd898aa4c562717cafd822f
ghoshdebashree <shree19ghosh@gmail.com>
2021-07-29 16:10:50 +0530

    first version portfolio

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+
+window.onload = function() {
+  var typed = new Typed('#typed', {
+    strings: [
+      'I am a Front End Developer from India.'
+    ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    delaySpeed:500,
+    smartBackspace: false,
+    showCursor: false,
+    loop:true
+  });
+};
