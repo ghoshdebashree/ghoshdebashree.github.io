b5950a9d761a05bf8161b1fad113b5f85b578b5b
D.e.b.a.s.h.r.e.e <59219070+ghoshdebashree@users.noreply.github.com>
2024-02-14 20:01:16 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 window.onload = function() {
   var typed = new Typed('#typed', {
     strings: [
-      'I am a Full Stack Developer from India.'
+      'I am a Java Full Stack Developer from India.'
     ],
     typeSpeed: 100,
     backSpeed: 50,
